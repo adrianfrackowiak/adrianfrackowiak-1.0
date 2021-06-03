8649ca2e17b63656acb9cab44d19f1bbc9985bfd
adrianfrackowiak <adrianfrackowiak@icloud.com>
2021-06-03 21:27:45 +0200

    make footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>created by adrianfrackowiak</p>
+        </footer>
+    );
+};
+
+export default Footer;
